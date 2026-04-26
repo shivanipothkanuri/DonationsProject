0efb4b41933988d06c5f4a285d47b9985c09794d
shivanipothkanuri <200807702+shivanipothkanuri@users.noreply.github.com>
2026-04-25 22:55:44 -0500

    Fix PageImpl serialization warning with EnableSpringDataWebSupport VIA_DTO

--- src/main/java/org/charityaid/charity_aid/CharityAidApplication.java
+++ src/main/java/org/charityaid/charity_aid/CharityAidApplication.java
@@ -2,12 +2,15 @@ package org.charityaid.charity_aid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class CharityAidApplication {
 
 	public static void main(String[] args) {
